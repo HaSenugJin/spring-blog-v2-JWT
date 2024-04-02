1e1f8635576481a2adc1098a27dd083d090e2302
hyeon <lhyeonnit@gmail.com>
2024-04-02 12:53:32 +0900

    jwt

---
+++ src/test/java/shop/mtcoding/blog/_core/utils/JwtUtilTest.java
@@ -0,0 +1,22 @@
+package shop.mtcoding.blog._core.utils;
+
+
+import org.junit.jupiter.api.Test;
+import shop.mtcoding.blog.user.User;
+
+public class JwtUtilTest {
+
+    @Test
+    public void create_test() {
+
+        // given
+        User user = User.builder()
+                .id(1)
+                .username("ssar")
+                .build();
+
+        String jwt = JwtUtil.create(user);
+
+        System.out.println(jwt);
+    }
+}
